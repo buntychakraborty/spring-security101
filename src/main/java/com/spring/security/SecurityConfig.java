49609b3c093f61ab5287ef41995b6650b0bb2123
Bunty Chakraborty <buntychakraborty@outlook.com>
2021-09-22 01:05:46 +0530

    custom auth provider

--- src/main/java/com/spring/security/SecurityConfig.java
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -5,23 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//@Configuration
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
 	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	
+	private MyAuthProvider authenticationProvider;
+
+//	@Autowired
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+//	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		InMemoryUserDetailsManager detailsManager = new InMemoryUserDetailsManager();
+//		detailsManager.createUser(User.withUsername("Bunty").password(bCryptPasswordEncoder.encode("12345")).authorities("read").build());
+//		auth.userDetailsService(detailsManager).passwordEncoder(bCryptPasswordEncoder);
+//
+//	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		InMemoryUserDetailsManager detailsManager = new InMemoryUserDetailsManager();
-		detailsManager.createUser(User.withUsername("Bunty").password(bCryptPasswordEncoder.encode("12345")).authorities("read").build());
-		auth.userDetailsService(detailsManager).passwordEncoder(bCryptPasswordEncoder);
+		auth.authenticationProvider(authenticationProvider);
 
 	}
 
@@ -31,7 +39,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().anyRequest().authenticated();
 	}
 
-	
 	@Bean
 	public BCryptPasswordEncoder gBCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
